2a6ef3f4820ec98f658b559fbcff269fab22a1f3
giacomozanatta <giacomo12596@gmail.com>
2018-01-23 23:16:52 +0100

    a

--- Src/unfinitaly/app/src/main/java/it/unive/dais/bunnyteam/unfinitaly/app/PercentageClusterRenderer.java
+++ Src/unfinitaly/app/src/main/java/it/unive/dais/bunnyteam/unfinitaly/app/PercentageClusterRenderer.java
@@ -20,6 +20,44 @@ public class PercentageClusterRenderer<T extends MapMarker> extends ClusterRende
         super(context, map, clusterManager);
     }
 
+    /* test, colorare il cluster in base alle percentuali medie @Override
+    protected void onBeforeClusterRendered(Cluster cluster, MarkerOptions markerOptions) {
+        super.onBeforeClusterRendered(cluster, markerOptions);
+        int size = cluster.getSize();
+        double sum = 0;
+        double media = 0;
+
+        int bucket = this.getBucket(cluster);
+        Collection<MapMarker> mM = cluster.getItems();
+        BitmapDescriptor descriptor = (BitmapDescriptor)this.mIcons.get(bucket);
+        if(descriptor == null) {
+
+            //descriptor = BitmapDescriptorFactory.fromBitmap(this.mIconGenerator.makeIcon(String.valueOf(cluster.getSize()))); //real numbers
+            descriptor = BitmapDescriptorFactory.fromBitmap(this.mIconGenerator.makeIcon(this.getClusterText(bucket))); //number +
+            this.mIcons.put(bucket, descriptor);
+        }
+        for(MapMarker m : mM)
+            sum += m.getPercentage();
+        media = sum / size;
+        if(media >= 0 && media < 25){
+            //Tra 0 e 25
+            this.mColoredCircleBackground.getPaint().setColor(this.getColor(Color.RED));
+        }else{
+            if(media >= 25 && media < 50){
+                //Tra 25 e 50
+                this.mColoredCircleBackground.getPaint().setColor(this.getColor(Color.RED));
+            }else{
+                if(media >= 50 && media < 75){
+                    //Tra 50 e 75
+                    this.mColoredCircleBackground.getPaint().setColor(this.getColor(Color.YELLOW));
+                }
+                else{
+                    //Tra 75 e 100
+                    this.mColoredCircleBackground.getPaint().setColor(this.getColor(Color.GREEN));
+                }
+            }
+        }
+    }*/
     @Override
     protected void onBeforeClusterItemRendered(MapMarker item, MarkerOptions markerOptions) {
         double percentage =  item.getPercentage();
