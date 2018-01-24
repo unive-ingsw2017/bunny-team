5ddc7ffa7d285e2676b6606450d9d94090e6c5b4
giacomozanatta <giacomo12596@gmail.com>
2018-01-24 10:06:25 +0100

    fix marker info activity

--- Src/unfinitaly/app/src/main/java/it/unive/dais/bunnyteam/unfinitaly/app/MapsActivity.java
+++ Src/unfinitaly/app/src/main/java/it/unive/dais/bunnyteam/unfinitaly/app/MapsActivity.java
@@ -6,14 +6,13 @@ package it.unive.dais.bunnyteam.unfinitaly.app;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -21,7 +20,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -29,7 +28,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
+
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -51,35 +50,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.maps.android.clustering.Cluster;
-import com.google.maps.android.clustering.ClusterManager;
-import com.google.maps.android.clustering.view.ClusterRenderer;
-import com.google.maps.android.clustering.view.DefaultClusterRenderer;
-import com.mikepenz.materialdrawer.AccountHeader;
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-
-import it.unive.dais.bunnyteam.unfinitaly.lib.parser.AsyncParser;
-import it.unive.dais.bunnyteam.unfinitaly.lib.parser.CsvRowParser;
-import it.unive.dais.bunnyteam.unfinitaly.lib.util.MapItem;
 
 /**
  * Questa classe è la componente principale del toolkit: fornisce servizi primari per un'app basata su Google Maps, tra cui localizzazione, pulsanti
@@ -564,8 +536,6 @@ public class MapsActivity extends BaseActivity
         });
     }
 
-    @Nullable
-    private Collection<Marker> markers;
 
     public void showMarkerInfo(MapMarker mapMarker){
         Intent i = new Intent(this, MarkerInfoActivity.class);
